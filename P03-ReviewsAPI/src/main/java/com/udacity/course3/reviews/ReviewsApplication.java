bc25d333692bd9da92159a933d102152f41472b3
Priya <priya.laurel@gmail.com>
2019-12-25 22:53:27 -0500

    fix: Fixed the bean initialization errors on enabling polyglot persistance
    Adding @EnableMongoRepositories and @EnableJpaRepositories with basePackageClasses solved the bean initialization error.

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/ReviewsApplication.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/ReviewsApplication.java
@@ -1,13 +1,25 @@
 package com.udacity.course3.reviews;
 
+
+import com.udacity.course3.reviews.mongoRepository.ReviewMongoRepository;
+import com.udacity.course3.reviews.repository.CommentRepository;
+import com.udacity.course3.reviews.repository.ProductRepository;
+import com.udacity.course3.reviews.repository.ReviewRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = ReviewMongoRepository.class)
+@EnableJpaRepositories(basePackageClasses = {ReviewRepository.class, ProductRepository.class, CommentRepository.class})
 public class ReviewsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewsApplication.class, args);
 	}
+
+
+
 
 }
