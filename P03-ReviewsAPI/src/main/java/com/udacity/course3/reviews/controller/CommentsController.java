1f91a189e109c4a419ebb3f7b98cb835c8338eaa
Priya <priya.laurel@gmail.com>
2020-01-02 14:51:20 -0500

    feat : Added sorting feature and updated controller to save comments to MySQL and MongoDB
    src/main/java/com/udacity/course3/reviews/controller/CommentsController.java - Added parameter for sorting comments for review.
                - Updated controller to save comments to related Review
                   document in MongoDB also

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/controller/CommentsController.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/controller/CommentsController.java
@@ -1,15 +1,19 @@
 package com.udacity.course3.reviews.controller;
 
+import com.udacity.course3.reviews.document.Review;
 import com.udacity.course3.reviews.exception.ResourceNotFoundException;
 import com.udacity.course3.reviews.model.Comment;
+import com.udacity.course3.reviews.mongoRepository.ReviewMongoRepository;
 import com.udacity.course3.reviews.repository.CommentRepository;
 import com.udacity.course3.reviews.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring REST controller for working with comment entity.
@@ -22,6 +26,9 @@ public class CommentsController {
 
     @Autowired
     private ReviewRepository reviewRepository;
+
+    @Autowired
+    private ReviewMongoRepository reviewMongoRepository;
 
     @Autowired
     private CommentRepository commentRepository;
@@ -40,7 +47,10 @@ public class CommentsController {
     public ResponseEntity<Comment> createCommentForReview(@PathVariable("reviewId") Long reviewId, @Valid @RequestBody Comment comment) {
         Comment savedComment = reviewRepository.findById(reviewId).map(review -> {
             comment.setReview(review);
-            return commentRepository.save(comment);
+            Comment newComment = commentRepository.save(comment);
+            //update Review document to add comment in MongoDB
+            updateCommentForReview(newComment,reviewId);
+            return newComment;
         }).orElseThrow(() -> new ResourceNotFoundException("ReviewID " + reviewId + " not found"));
 
         return ResponseEntity.ok(savedComment);
@@ -56,7 +66,34 @@ public class CommentsController {
      * @param reviewId The id of the review.
      */
     @RequestMapping(value = "/reviews/{reviewId}", method = RequestMethod.GET)
-    public List<Comment> listCommentsForReview(@PathVariable("reviewId") Long reviewId) {
-        return commentRepository.findByReviewId(reviewId);
+    public List<Comment> listCommentsForReview(@PathVariable("reviewId") Long reviewId,@RequestParam(defaultValue = "1") Boolean mostRecent) {
+
+        if(mostRecent!=null && mostRecent){
+            return commentRepository.findByReviewId(reviewId,Sort.by("created").descending());
+        }else{
+            return commentRepository.findByReviewId(reviewId,Sort.by("created").ascending());
+        }
+    }
+
+
+    /**
+     * Update review document in MongoDB when new comments are added.
+     *
+     * @param comment The MySQL comment.
+     * @return void.
+     */
+    private void updateCommentForReview(Comment comment,Long reviewId){
+
+        com.udacity.course3.reviews.document.Comment mongoComment= new com.udacity.course3.reviews.document.Comment();
+        mongoComment.setUserName(comment.getUserName());
+        mongoComment.setUserEmail(comment.getUserEmail());
+        mongoComment.setText(comment.getText());
+        mongoComment.setCreated(comment.getCreated());
+
+        Optional<Review> optionalReview = reviewMongoRepository.findById(reviewId);
+        optionalReview.ifPresent(review -> {
+            review.getComments().add(mongoComment);
+            reviewMongoRepository.save(review);
+        });
     }
 }
